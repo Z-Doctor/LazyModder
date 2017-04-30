8d62bc6c06b480ed9be65ecb3014c0e04bd8b0cd
Reuben Renquist <seedofdiscovery@gmail.com>
2017-04-29 19:52:29 -0700

    Added new shaped recipe

---
+++ src/main/java/zdoctor/lazymodder/inventory/NoContainerRecipes.java
@@ -0,0 +1,98 @@
+package zdoctor.lazymodder.inventory;
+
+import java.util.Map;
+
+import com.google.common.collect.Maps;
+
+import net.minecraft.block.Block;
+import net.minecraft.inventory.InventoryCrafting;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.ShapedRecipes;
+import net.minecraft.util.NonNullList;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
+public class NoContainerRecipes extends ShapedRecipes {
+
+		private ItemStack container = ItemStack.EMPTY;
+
+		public NoContainerRecipes(int width, int height, ItemStack[] ingredientsIn, ItemStack output) {
+			super(width, height, ingredientsIn, output);
+		}
+		
+		public NoContainerRecipes setCustomContainer(ItemStack stack) {
+			this.container  = stack;
+			return this;
+		}
+
+		@Override
+		public NonNullList<ItemStack> getRemainingItems(InventoryCrafting inv) {
+			NonNullList<ItemStack> nonnulllist = NonNullList.<ItemStack>withSize(inv.getSizeInventory(),
+					ItemStack.EMPTY);
+
+			for (int i = 0; i < nonnulllist.size(); ++i) {
+				ItemStack itemstack = inv.getStackInSlot(i);
+				nonnulllist.set(i, this.container);
+			}
+
+			return nonnulllist;
+		}
+
+		public static NoContainerRecipes addRecipe(ItemStack stack, Object... recipeComponents) {
+			String s = "";
+			int i = 0;
+			int j = 0;
+			int k = 0;
+
+			if (recipeComponents[i] instanceof String[]) {
+				String[] astring = (String[]) ((String[]) recipeComponents[i++]);
+
+				for (String s2 : astring) {
+					++k;
+					j = s2.length();
+					s = s + s2;
+				}
+			} else {
+				while (recipeComponents[i] instanceof String) {
+					String s1 = (String) recipeComponents[i++];
+					++k;
+					j = s1.length();
+					s = s + s1;
+				}
+			}
+
+			Map<Character, ItemStack> map;
+
+			for (map = Maps.<Character, ItemStack>newHashMap(); i < recipeComponents.length; i += 2) {
+				Character character = (Character) recipeComponents[i];
+				ItemStack itemstack = ItemStack.EMPTY;
+
+				if (recipeComponents[i + 1] instanceof Item) {
+					itemstack = new ItemStack((Item) recipeComponents[i + 1]);
+				} else if (recipeComponents[i + 1] instanceof Block) {
+					itemstack = new ItemStack((Block) recipeComponents[i + 1], 1, 32767);
+				} else if (recipeComponents[i + 1] instanceof ItemStack) {
+					itemstack = (ItemStack) recipeComponents[i + 1];
+				}
+
+				map.put(character, itemstack);
+			}
+
+			ItemStack[] aitemstack = new ItemStack[j * k];
+
+			for (int l = 0; l < j * k; ++l) {
+				char c0 = s.charAt(l);
+
+				if (map.containsKey(Character.valueOf(c0))) {
+					aitemstack[l] = ((ItemStack) map.get(Character.valueOf(c0))).copy();
+				} else {
+					aitemstack[l] = ItemStack.EMPTY;
+				}
+			}
+
+			NoContainerRecipes noContainerRecipes = new NoContainerRecipes(j, k, aitemstack, stack);
+			GameRegistry.addRecipe(noContainerRecipes);
+			return noContainerRecipes;
+		}
+
+	}
