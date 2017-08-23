7195b0e6a49a6720d0b3cfb87a1517e058dcd7ca
Reuben Renquist <seedofdiscovery@gmail.com>
2017-08-23 14:49:59 -0700

    Removed unused data

--- src/main/dev/zdoctor/lazymodder/easy/Constants.java
+++
@@ -1,5 +0,0 @@
-package zdoctor.lazymodder.easy;
-
-public final class Constants {
-	public static final double DEFUALT_SPEED = 0.10000000149011612D;
-}
--- src/main/test/zdoctor/lazymodder/examples/test/TestEntity.java
+++
@@ -1,9 +0,0 @@
-package zdoctor.lazymodder.examples.test;
-
-import zdoctor.lazymodder.easy.entity.EasyEntity.EasyTileEntity.UpdateTileEntity;
-
-public class TestEntity extends UpdateTileEntity {
-	@Override
-	public void update() {
-	}
-}
